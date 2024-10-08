e8f87f898a3c2098b14263cab5639f234627834e
Fellyla Fiorenza <115087764+felyoww@users.noreply.github.com>
2024-10-08 17:40:38 +0700

    Update web.php

--- pbkk-datingapp/routes/web.php
+++ pbkk-datingapp/routes/web.php
@@ -6,7 +6,23 @@ Route::get('/', function () {
     return view('home');
 });
 
-
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+// Ensure the entire block is commented out
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/project', function () {
+    return view('project');
 });
